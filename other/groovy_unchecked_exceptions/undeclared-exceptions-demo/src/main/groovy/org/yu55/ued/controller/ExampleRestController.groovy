094bf7c0e13e69d7bc319e8cf6ec1766d3ed5679
Marcin Pilaczynski <marcin.pil@gmail.com>
2016-02-04 20:16:22 +0100

    Comment in exception

--- other/groovy_unchecked_exceptions/undeclared-exceptions-demo/src/main/groovy/org/yu55/ued/controller/ExampleRestController.groovy
+++ other/groovy_unchecked_exceptions/undeclared-exceptions-demo/src/main/groovy/org/yu55/ued/controller/ExampleRestController.groovy
@@ -8,6 +8,10 @@ class ExampleRestController {
 
     @RequestMapping(value = '/get')
     String get() {
+        /*
+         Lets imagine this exception is thrown somewhere from deepest layers of our service code
+         and we don't have to be immediately aware of that.
+          */
         throw new ExampleRestControllerException()
     }
 }
