c25bfc489cca6b0b86fe6b6c0d21b831df8cb7ae
Marcin Pilaczynski <marcin.pil@gmail.com>
2016-05-18 21:42:57 +0200

    Variable might not have been initialized

---
+++ 1Z0-808/Sandard Tests/SAMPLE TEST/Discounter.java
@@ -0,0 +1,19 @@
+public class Discounter {
+
+    static double percent; //1
+    int offset = 10, base= 50; //2
+
+    public static double calc(double value) {
+        int coupon, offset, base; //3
+        if(percent <10){ //4
+            coupon = 15;
+            offset = 20;
+            base = 10;
+        }
+        return coupon*offset*base*value/100; //5
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calc(100));
+    }
+}
