7b757009db381eeccef865283a9908a65403463c
Marcin Pilaczynski <marcin.pil@gmail.com>
2016-06-01 07:41:43 +0200

    Overloaded methods matching order - upgraded again

--- 1Z0-808/Sandard Tests/SAMPLE TEST/OverloadedMethodsMatchingOrderTest.java
+++ 1Z0-808/Sandard Tests/SAMPLE TEST/OverloadedMethodsMatchingOrderTest.java
@@ -30,12 +30,24 @@ public class OverloadedMethodsMatchingOrderTest {
         System.out.println("double double");
     }
 
+    public void glide(boolean i, boolean j) { // won't be matched
+        System.out.println("boolean boolean");
+    }
+
+    public void glide(char i, char j) { // won't be matched
+        System.out.println("char char");
+    }
+
     public void glide(Byte i, Byte j) {
         System.out.println("Byte Byte");
     }
 
-    public void glide(byte... ij) {
-        System.out.println("byte...");
+    public void glide(Short i, Short j) { // won't be matched
+        System.out.println("Short Short");
+    }
+
+    public void glide(Object i, Object j) {
+        System.out.println("Object Object");
     }
 
     public void glide(short... ij) {
@@ -58,20 +70,28 @@ public class OverloadedMethodsMatchingOrderTest {
         System.out.println("double...");
     }
 
-    public void glide(Object i, Object j) {
-        System.out.println("Object Object");
+    public void glide(Byte... ij) {
+        System.out.println("Byte...");
     }
 
-    public void glide(Short i, Short j) { // won't be matched
-        System.out.println("Short Short");
+    public void glide(Short... ij) { // won't be matched
+        System.out.println("Short...");
     }
 
-    public void glide(boolean i, boolean j) { // won't be matched
-        System.out.println("boolean boolean");
+    public void glide(Integer... ij) { // won't be matched
+        System.out.println("Integer...");
     }
 
-    public void glide(char i, char j) { // won't be matched
-        System.out.println("char char");
+    public void glide(Long... ij) { // won't be matched
+        System.out.println("Long...");
+    }
+
+    public void glide(Float... ij) { // won't be matched
+        System.out.println("Float...");
+    }
+
+    public void glide(Double... ij) { // won't be matched
+        System.out.println("Double...");
     }
 
 }
