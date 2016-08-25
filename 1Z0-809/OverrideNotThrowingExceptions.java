ee91e5c45f0a9653bbe0497e9996c3d8007bc723
Marcin Pilaczynski <marcin.pil@gmail.com>
2016-08-25 20:55:24 +0200

    Renaming class name

--- 1Z0-809/OverrideNotThrowingExceptions.java
+++ 1Z0-809/OverrideNotThrowingExceptions.java
@@ -14,7 +14,7 @@ class B extends A
     }
 }
 
-class OverridenNotThrowingExceptions {
+class OverrideNotThrowingExceptions {
     public static void main(String[] args) {
         System.out.println("I did compiled!");
     }
