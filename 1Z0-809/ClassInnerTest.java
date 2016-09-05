dfefd4cfb77ee83fe5406cb1837bc159fb725c67
Marcin Pilaczynski <marcin.pil@gmail.com>
2016-09-05 20:11:35 +0200

    Inner class: added some trick variable naming

--- 1Z0-809/ClassInnerTest.java
+++ 1Z0-809/ClassInnerTest.java
@@ -1,16 +1,20 @@
 class OuterClass {
 
   private static int outerStaticField;
-
   private int outerInstanceField;
-
+  private int i = 77;
 
   class InnerClass {
-    static final int innerField = 0; // static without final is NOT LEGAL
+    static final int innerField = 0;       // static without final is NOT LEGAL
     private int innerInstanceField = 1;
+    private int i = 55;
     void method() {
-      outerInstanceField = 2; // full access
-      outerStaticField = 3; // full access
+      outerInstanceField = 2;              // full access
+      outerStaticField = 3;                // full access
+      
+      System.out.println("i=" + i);                                 // prints 55 (or 77 if "private int i = 77" doesn't exists
+      System.out.println("this.i=" + this.i);                       // prints 55
+      System.out.println("OuterClass.this.i" + OuterClass.this.i);  // prints 77
     }
   }
 
@@ -28,7 +32,7 @@ class ClassInnerTest {
   public static void main(String[] args) {
 
     OuterClass.InnerClass ic = new OuterClass().new InnerClass(); // NOT LIKE THIS: new OuterClass.InnerClass();
-
+    ic.method();
     OuterClass.InnerInterface ii = new OuterClass.InnerInterface() {}; //NOT LIKE THIS: new OuterClass().new InnerInterface() {};
 
   }
